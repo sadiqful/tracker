01a528372af9e1417c82bb281faecc24aaa602a7
Aliyu <sadiqful@gmail.com>
2023-10-22 23:45:52 +0100

    page: added badge to status page

--- app/components/IssueStatusBadge.tsx
+++ app/components/IssueStatusBadge.tsx
@@ -7,8 +7,8 @@ Status,
         {label: string, color: 'red' | 'violet' | 'green'}
 > = {
         OPEN: {label: 'Open', color: 'red'},
-        IN_PROGRESS: {label: 'Open', color: 'red'},
-        CLOSED: {label: 'Open', color: 'red'}
+        IN_PROGRESS: {label: 'Open', color: 'violet'},
+        CLOSED: {label: 'Open', color: 'green'}
 };
 
 const IssueStatusBadge = ({status}:{status: Status}) => {
