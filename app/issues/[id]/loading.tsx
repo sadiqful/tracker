fa94213f68f9413c03d76b879759144a6d5953b9
Aliyu <sadiqful@gmail.com>
2023-10-27 23:01:07 +0100

    refactor: organized imports

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,5 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import { Heading, Card, Flex, Box } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
+import { Skeleton } from '@/app/components'
 
 const LoadingIssueDetailPage = () => {
   return (
