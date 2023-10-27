46274ee4d61b9685d2df1c46b80869fd09274193
Aliyu <sadiqful@gmail.com>
2023-10-27 22:38:51 +0100

    loading skeletons for issues

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,15 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import { Heading, Card, Flex, Box } from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingNewIssuePage = () => {
   return (
-    <div>loading...</div>
+    <Box className='max-x-wl'>
+      <Skeleton />
+      <Skeleton height='20rem' />
+    </Box>
   )
 }
 
