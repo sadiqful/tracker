9bb0ac12b46d57366f2664c550ec272418aa999d
Aliyu <sadiqful@gmail.com>
2023-10-27 10:09:35 +0100

    get issue detail

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -24,7 +24,9 @@ await delay(2000);
           {issues.map(issue => (
           <Table.Row key={issue.id}>
             <Table.Cell>
+              <Link href={`/issues/${issue.id}`}>
               {issue.title}
+              </Link>
               <div className='block md:hidden'>
                 <IssueStatusBadge status={issue.status} />
               </div>
